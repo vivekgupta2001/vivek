dfd2f6778feacd2e5e4b94eb960b811b7995d77d
vivek <vivek.gupta2001@gmail.com>
2013-09-26 23:56:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonalAssistant::Application.config.secret_key_base = '0bafbb41bbe4e42e2f43fd0371a1b7e7bed7d3239eb8f52b3e2960b3a7190662a84beadd7778db6976300f240502037fcc238bf2c9c09ba274b322cfea5699f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalAssistant::Application.config.session_store :cookie_store, key: '_PersonalAssistant_session'
